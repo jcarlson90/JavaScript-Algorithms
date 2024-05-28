af6ab246139282745950cda3f6354dcf6c709e30
jcarlson <jake.carlson90@gmail.com>
2024-05-28 10:45:54 -0700

    minimum stack

--- dataStructures/mediumQs.js
+++ dataStructures/mediumQs.js
@@ -119,3 +119,33 @@ class Solution {
     }
 }
 
+// MINIMUM STACK
+
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+    push(val) {
+        this.stack.push(val);
+        val = Math.min(
+            val,
+            this.minStack.length === 0
+                ? val
+                : this.minStack[this.minStack.length - 1],
+        );
+        this.minStack.push(val);
+    }
+    pop() {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+    getMin() {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
+
