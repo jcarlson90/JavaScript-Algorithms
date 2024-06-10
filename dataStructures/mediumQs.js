99b1fb858f58f60261437bfe8de31e79313f08c6
jcarlson <jake.carlson90@gmail.com>
2024-06-10 16:08:27 -0700

    palindrome

--- dataStructures/mediumQs.js
+++ dataStructures/mediumQs.js
@@ -149,3 +149,19 @@ class MinStack {
     }
 }
 
+var isPalindrome = function(x) {
+    if (x < 0) {
+        return false;
+    }
+
+    let reverse = 0;
+    let xcopy = x;
+
+    while (x > 0) {
+        reverse = (reverse * 10) + (x % 10);
+        x = Math.floor(x / 10);
+    }
+
+    return reverse === xcopy;    
+};
+
