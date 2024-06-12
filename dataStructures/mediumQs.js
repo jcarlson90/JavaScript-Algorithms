f890d831a24ad6396313b68216fee04a4532bcd3
jcarlson <jake.carlson90@gmail.com>
2024-06-11 21:25:18 -0700

    climbing stairs

--- dataStructures/mediumQs.js
+++ dataStructures/mediumQs.js
@@ -215,3 +215,19 @@ var plusOne = function(digits) {
 
     return nums
 };
+
+var climbStairs = function(n) {
+    if (n <= 3) return n;
+
+    let prev1 = 3;
+    let prev2 = 2;
+    let cur = 0;
+
+    for (let i = 3; i < n; i++) {
+        cur = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = cur;
+    }
+
+    return cur;    
+};
