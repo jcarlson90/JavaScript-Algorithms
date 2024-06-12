c5f477fc5eb2b8699a20416e148c478df9b573c8
jcarlson <jake.carlson90@gmail.com>
2024-06-11 21:21:19 -0700

    plus one

--- dataStructures/mediumQs.js
+++ dataStructures/mediumQs.js
@@ -190,3 +190,28 @@ var lengthOfLastWord = function(s) {
     len = arr.length -1
     return (arr[len].length);
    };
+
+
+var plusOne = function(digits) {
+    // loop from right moving left
+    // add 1 and determin if you need to carry
+    let [carry, idx, nums] = [1, digits.length-1, [...digits]]
+
+    // as soon as you don't have to carry RETURN
+    while(carry){
+        const val = nums[idx] + carry
+        if( val === 10){
+            nums[idx] = 0
+            if(idx === 0){
+                nums.unshift(carry)
+                carry = 0
+                }
+        }else{
+            nums[idx] = val
+            carry = 0
+        }
+        idx--
+    }
+
+    return nums
+};
